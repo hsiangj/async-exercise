cff279c7f2fc78ee7ed23b8ba8a0b206b63f5eca
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-04-14 15:07:00 -0700

    Initial commit

--- part-1-number-facts/app.js
+++ part-1-number-facts/app.js
@@ -6,38 +6,45 @@ let baseURL = 'http://numbersapi.com'
 
 
 // 1. Make request to the Numbers API to get a fact about your favorite number
-axios.get(`${baseURL}/${favNum}/?json`)
-  .then(res => console.log(res))
-
+async function part1(){
+  let res = await axios.get(`${baseURL}/${favNum}/?json`)
+  console.log(res)
+}
+part1();
 
 // 2. Get data on multiple numbers in a single request 
-let favNums = [2,4,6]
-
-axios.get(`${baseURL}/${favNums}?json`)
-  .then(res => {
-    for (let num in res.data){
-      const fact = createFactHTML(res.data[num]);
-      numberFacts.append(fact);
-    }
-  })
-  .catch(err => console.log('Error', err))
+let favNums = [2,4,6,8]
+async function part2(){
+  let {data} = await axios.get(`${baseURL}/${favNums}?json`)
+  for (let num in data){
+    const fact = createFactHTML(data[num]);
+    numberFacts.append(fact);
+  }
+}
 
 function createFactHTML(fact){
   const li = document.createElement('li');
   li.append(fact);
   return li;
 }
+part2();
 
 // 3. Get 4 facts for favorite number
-Promise.all(Array.from({length:4}, () => {
-  return axios.get(`${baseURL}/${favNum}/?json`)
-}))
-  .then(res => {  
-    for (let fact of res){
-      const text = createFactHTML(fact.data.text);
-      favNumberFacts.append(text)
-    }
-  })
-  .catch(err => {
-    favNumberFacts.append('something went wrong', err)
-  })
+async function part3(){
+  let res = await Promise.all(
+    Array.from({length:4}, () => axios.get(`${baseURL}/${favNum}/?json`)))
+
+  // or
+  // [
+  //   axios.get(`${baseURL}/${favNum}/?json`),
+  //   axios.get(`${baseURL}/${favNum}/?json`),
+  //   axios.get(`${baseURL}/${favNum}/?json`),
+  //   axios.get(`${baseURL}/${favNum}/?json`)
+  // ]
+
+  for (let fact of res){
+    const text = createFactHTML(fact.data.text);
+    favNumberFacts.append(text)
+  }
+}
+part3();
--- part-2-deck-of-cards/app.js
+++ part-2-deck-of-cards/app.js
@@ -2,25 +2,27 @@ const baseURL = 'https://deckofcardsapi.com/api/deck'
 
 // 1. Make a request to the Deck of Cards API to request a single card from a newly shuffled deck. Once you have the card, console.log the value and the suit (e.g. “5 of spades”, “queen of diamonds”).
 
-// axios.get(`${baseURL}/new/draw/?count=1`)
-//   .then(res => {
-//     const {value, suit} = res.data.cards[0]
-//     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`)
-//   })
+// async function part1(){
+//   let res = await axios.get(`${baseURL}/new/draw`)
+//   const {value, suit} = res.data.cards[0]
+//   console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`)
+// }
+// part1();
 
 // 2. Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the same deck. Once you have both cards, console.log the values and suits of both cards.
 
-// let firstCard;
-// axios.get(`${baseURL}/new/draw/?count=1`)
-//   .then(res => {
-//     firstCard = res.data.cards[0];
-//     const deckID = res.data.deck_id
-//     return axios.get(`${baseURL}/${deckID}/draw/?count=1`)
-//   }).then(res => {
-//     let secondCard = res.data.cards[0];
-//     [firstCard, secondCard].forEach(card => 
-//       console.log(`${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`))
-//   })
+
+// async function part2(){
+//   let res = await axios.get(`${baseURL}/new/draw`);
+//   let firstCard = res.data.cards[0];
+//   const deckID = res.data.deck_id;
+//   let res2 = await axios.get(`${baseURL}/${deckID}/draw/?count=1`)
+//   let secondCard = res2.data.cards[0];
+//   [firstCard, secondCard].forEach(card => 
+//   console.log(`${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`))
+// }
+// part2();
+
 
   // 3. Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
 
@@ -28,24 +30,23 @@ let deckId;
 let button = document.querySelector('button')
 let cardArea = document.getElementById('card-area')
 
-button.addEventListener('click', function(){
-  drawCard()
-})
 
-axios.get(`${baseURL}/new/shuffle`)
-  .then(res => {
-    deckId = res.data.deck_id;
-  });
 
-function drawCard(){
-  axios.get(`${baseURL}/${deckId}/draw`)
-  .then(res => {
+async function part3(){
+  let res = await axios.get(`${baseURL}/new/shuffle`)
+  deckId = res.data.deck_id;
+
+  button.addEventListener('click', async function(){
+    let res = await axios.get(`${baseURL}/${deckId}/draw`)
     cardArea.innerHTML = "";
     let img = document.createElement('img');
     img.src = res.data.cards[0].image;
     cardArea.append(img);
+    
     if (res.data.remaining === 0){
       button.remove();
     }
   })
 }
+part3();
+
